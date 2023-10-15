86bb983476af21fed392e719b9fd1ea5265c461b
Demian Rymar <ua.demyan@gmail.com>
2023-10-15 18:00:02 +0300

    unittest

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LAB_5.0/Main.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = sum(2, 3);
+			Assert::AreEqual(t, 5);
+		}
+	};
+}
